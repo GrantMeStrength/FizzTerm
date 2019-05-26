4efeef40eaeb3733e3232f0a894725f7f44fd3f7
John Kennedy <jken@microsoft.com>
2019-05-25 18:55:14 -0700

    Upload of all the files.

---
+++ FizzTerm.ino
@@ -0,0 +1,465 @@
+/*
+  FizzTerm - A Simple Serial File System Manager
+
+  Use it to record and play back serial data to an SD card. It's particularly
+  useful if using a retro computer that communicates over RS232 and you
+  need to capture program listings to disk.
+
+  You can also access the SD card on your computer, edit them there,
+  and then load them back into your retro system.
+  
+  Requires an Aduino Mega or compatible, with multiple serial port support.
+
+  Assumes that MAX232-type adaptors are connected to the Arduino's default
+  Serial1 and Serial2 ports, and the SD Card reader is connected in the usual
+  way (although the Chip Select pin may depend on your hardware).
+
+  Note: In order to make LIST command work, you must duplicate the SD library
+  and change all the references to Serial. to Serial1. If you don't,
+  when you type LIST you won't see the list of files on your card.
+
+  Note: Baud rate is set at 9600. You may need to change this.
+  
+*/
+
+
+#include <SPI.h>
+#include <SD.h>
+
+// Styles
+
+#define GREEN "\033[0;32m"
+#define WHITE "\033[1;37m"
+
+// Set up variables using the SD utility library functions:
+Sd2Card card;
+SdVolume volume;
+SdFile root;
+const int chipSelect = 53;  // Might be different on your hardware
+
+// Variables used by this app
+int BAUDRATE = 9600;
+bool HIDE_ECHO = false;
+bool RECORDING = false;
+char buffer[80];
+char buffer_count = 0;
+File myFile;
+
+
+void setup() {
+
+  //Serial.begin(BAUDRATE); // For debugging
+  Serial1.begin(BAUDRATE); // The Terminal
+  Serial2.begin(BAUDRATE); // The Computer
+
+  Serial1.println(GREEN);
+  Serial1.println("  ______ _      _______                  ");
+  Serial1.println(" |  ____(_)    |__   __|                 ");
+  Serial1.println(" | |__   _ _______| | ___ _ __ _ __ ___  ");
+  Serial1.println(" |  __| | |_  /_  / |/ _ \\ '__| '_ \\` _ \\ ");
+  Serial1.println(" | |    | |/ / / /| |  __/ |  | | | | | |");
+  Serial1.println(" |_|    |_/___/___|_|\\___|_|  |_| |_| |_|\n");
+  Serial1.println("                                      v0.1\n");
+  Serial1.println(WHITE);
+
+  if (!card.init(SPI_HALF_SPEED, chipSelect)) {
+    Serial.println("Error: SD Card was not found.");
+    while (1);
+  }
+
+  // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
+  if (!volume.init(card)) {
+    Serial.println("Error: SD Card was found, but could not be read.");
+    while (1);
+  }
+
+  // Clear buffer
+  for (int i = 0; i < 80; i++)
+    buffer[i] = 0;
+}
+
+
+// Let the user enter a line of text from the terminal, and echo it back to the terminal.
+// If it's a backspace, delete the character previous entered.
+// Check for <0 or >80 characters.
+// When the user enters NEWLINE, well, then we do stuff.
+// If you wanted to add support for cursor up and down to go through a history, then
+// this is where you would do it.
+
+void Enter_Line()
+{
+  if (Serial1.available())
+  {
+
+    // Read in and echo back to terminal. Don't send to computer yet.
+
+    int inByte = Serial1.read();
+    Serial1.write(inByte);
+    buffer[buffer_count] = inByte;
+    bool found_command = false;
+
+    if (inByte == 3 && RECORDING)
+    {
+      RECORDING = false;
+      myFile.close();
+      Serial1.print(GREEN);
+      Serial1.println("Saving complete.");
+      Serial1.print(WHITE);
+    }
+
+
+    if (inByte == 8) // User pressed delete
+    {
+      buffer_count--;
+      if (buffer_count < 0)
+      {
+        buffer_count = 0;
+      }
+    }
+    else
+    {
+      buffer_count++;
+      if (buffer_count >= 80)
+      {
+        buffer_count = 79;
+      }
+    }
+
+    if (inByte == 13) // User pressed return
+    {
+      // Check for commmands
+
+      buffer[buffer_count - 1] = 0; // Get rid of the newline character in there
+
+      if (strncmp(buffer, "!HELP", 5) == 0)
+      {
+        FS_HELP();
+        found_command = true;
+      }
+
+      if (strncmp(buffer, "!LIST", 5) == 0)
+      {
+        FS_LIST();
+        found_command = true;
+      }
+
+      if (strncmp(buffer, "!SAVE", 5) == 0)
+      {
+        char filename[13];
+        for (int i = 6; i < 18; i++)
+          filename[i - 6] = buffer[i];
+        filename[12] = 0;
+        FS_SAVE(filename);
+        found_command = true;
+      }
+
+      if (strncmp(buffer, "!LOAD", 5) == 0)
+      {
+        char filename[13];
+        for (int i = 6; i < 18; i++)
+          filename[i - 6] = buffer[i];
+        filename[12] = 0;
+        FS_LOAD(filename);
+        found_command = true;
+      }
+
+
+      if (strncmp(buffer, "!WIPE", 4) == 0)
+      {
+        char filename[13];
+        for (int i = 6; i < 18; i++)
+          filename[i - 6] = buffer[i];
+        filename[12] = 0;
+        FS_WIPE(filename);
+
+        found_command = true;
+      }
+
+      if (strncmp(buffer, "!DUMP", 5) == 0)
+      {
+        char filename[13];
+        for (int i = 6; i < 18; i++)
+          filename[i - 6] = buffer[i];
+        filename[12] = 0;
+        FS_DUMP(filename);
+        found_command = true;
+      }
+
+      if (strncmp(buffer, "!STOP", 5) == 0)
+      {
+
+        if (RECORDING)
+        {
+          RECORDING = false;
+          myFile.close();
+          Serial1.print(GREEN);
+          Serial1.println("Saving complete.");
+          Serial1.println(WHITE);
+        }
+        else
+        {
+          Serial1.print(GREEN);
+          Serial1.println("Error: Nothing was being saved.");
+          Serial1.println(WHITE);
+        }
+
+        found_command = true;
+      }
+
+      if (found_command == true)
+      {
+        buffer_count = 0;
+        for (int i = 0; i < 80; i++)
+          buffer[i] = 0;
+        return;
+      }
+
+      buffer[buffer_count - 1] = 13; // Add back the newline character so computer acts on text
+
+      // No commands found, so a regular string to pass onto computer
+
+      HIDE_ECHO = true; // Don't display the text the computer spits back that includes the line we just sent.
+      // Why? Sometimes there is a > prompt for example, and that would mess up what the user
+      // sees.
+
+      Serial1.write(13);
+      Serial2.write(buffer);
+      buffer_count = 0;
+      for (int i = 0; i < 80; i++)
+        buffer[i] = 0;
+    }
+  }
+}
+
+// This displays the information coming from the computer. It's also where we save it to the SD
+// card if that option is currenly active.
+
+void Display_From_Computer()
+{
+  if (Serial2.available())
+  {
+
+    // Read in and echo back
+    int inByte = Serial2.read();
+
+    if (HIDE_ECHO == false)
+    {
+      Serial1.write(inByte);
+
+      if (RECORDING) // Save any data from the computer to an open file on the SD card.
+      {
+        myFile.write(inByte);
+      }
+
+    }
+
+    if (inByte == 13) // Wait to echo text back until we get the CR, so absorb the echo of the sent string
+    {
+      HIDE_ECHO = false;
+    }
+  }
+}
+
+void loop()
+{
+  Enter_Line();
+  Display_From_Computer();
+}
+
+
+
+
+void FS_HELP()
+{
+  Serial1.println(GREEN);
+  Serial1.println("FIZZTERM");
+  Serial1.println("!HELP       This helpful text.");
+  Serial1.println("!LIST       List the files stored on the SD card.");
+  Serial1.println("!LOAD file  Load the named file, sending it directly to the connected computer.");
+  Serial1.println("!DUMP file  Display the contents the named file, but only to the terminal.");
+  Serial1.println("!SAVE file  Save the next set of data to SD from the connected computer.");
+  Serial1.println("!STOP       Stop saving data to file.");
+  Serial1.println("!WIPE file   Delete the named file from the SD card.");
+  Serial1.println(WHITE);
+}
+
+
+void FS_DUMP(const char * filename)
+{
+
+  Serial1.println(GREEN);
+
+  if (!SD.begin(chipSelect))
+  {
+    Serial1.println("Error: Card failed, or not present");
+    Serial1.println(WHITE);
+    return;
+  }
+
+  if (!SD.exists(filename))
+  {
+    Serial1.println("Error: File not found");
+    Serial1.println(WHITE);
+    return;
+  }
+
+  File dataFile = SD.open(filename);
+
+  // if the file is available, read from it
+  if (dataFile)
+  {
+    while (dataFile.available())
+    {
+      Serial1.write(dataFile.read());
+    }
+    dataFile.close();
+  }
+  else
+  {
+    Serial1.println("Error: Could not open file");
+  }
+  Serial1.println(WHITE);
+}
+
+
+void FS_LOAD(const char* filename)
+{
+
+  Serial1.println(GREEN);
+
+  if (!SD.begin(chipSelect))
+  {
+    Serial1.println("Error: Card failed, or not present");
+    Serial1.println(WHITE);
+    return;
+  }
+
+  if (!SD.exists(filename))
+  {
+    Serial1.println("Error: File not found");
+    Serial1.println(WHITE);
+    return;
+  }
+
+
+
+  File dataFile = SD.open(filename);
+
+  Serial1.write("Loading..");
+
+  // if the file is available, read from it
+  if (dataFile) {
+    while (dataFile.available()) {
+      Serial2.write(dataFile.read());
+      delay(10); // Little pause to give the computer time to digest the data.
+
+      // Ignore any echo'd chars from computer
+      if (Serial2.available()) {
+        char inByte = Serial2.read();
+      }
+    }
+    dataFile.close();
+  }
+
+  // if the file isn't open, display error
+  else {
+    Serial1.println("Error: Could not open file");
+    Serial1.println(WHITE);
+    return;
+  }
+
+  Serial1.write("OK");
+  Serial1.println(WHITE);
+
+}
+
+void FS_LIST()
+{
+
+  Serial1.println(GREEN);
+
+  if (!SD.begin(chipSelect)) {
+    Serial1.println("Error: Card failed, or not present");
+    Serial1.println(WHITE);
+    return;
+  }
+
+
+  Serial1.println("Card contents (name, date and size in bytes):");
+  root.openRoot(volume);
+
+  // list all files in the card with date and size
+  root.ls(LS_R | LS_DATE | LS_SIZE);
+
+  // Turn off the green style
+  Serial1.println(WHITE);
+
+}
+
+void FS_WIPE(const char *filename)
+{
+  Serial1.println(GREEN);
+
+  if (!SD.begin(chipSelect)) {
+    Serial1.println("Error: Card failed, or not present");
+    Serial1.println(WHITE);
+    return;
+  }
+
+  if (!SD.exists(filename))
+  {
+
+    Serial1.print("Error: File not found");
+    Serial1.println(WHITE);
+    return;
+  }
+
+
+  bool fail = !SD.remove(filename);
+
+  if (fail)
+  {
+    Serial1.println("Error: file could not be deleted");
+  }
+  else
+  {
+    Serial1.println("File deleted");
+  }
+
+  Serial1.println(WHITE);
+
+
+}
+
+void FS_SAVE(const char* filename)
+{
+
+  Serial1.println(GREEN);
+
+  if (!SD.begin(chipSelect)) {
+    Serial1.println("Error: Card failed, or not present");
+    Serial1.println(WHITE);
+    return;
+  }
+
+  if (strlen(filename) < 1)
+  {
+    Serial1.println("Error: missing filename");
+    Serial1.println(WHITE);
+    return;
+  }
+
+  myFile = SD.open(filename, O_WRITE | O_CREAT);
+
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial1.println("Saving data to card. Stop with CTRL C or !STOP");
+    Serial1.println(WHITE);
+    RECORDING = true;
+
+  } else {
+    // if the file didn't open, print an error:
+    Serial1.println("Error: Could not create the file");
+    Serial1.println(WHITE);
+  }
+}
---
+++ ReadMe.md
@@ -0,0 +1,65 @@
+# FizzTerm
+
+## A simple Arduino-based system for capturing and replaying serial data to SD card.
+
+![](FizzTerm_snap.jpg)
+
+If you have built a retro-computer project from a kit, say, an Altair-Duino or an RC2014, you might be looking for a convenient way to load and save content.
+
+This project creates a device that sits between your retro-computer and your terminal's RS232 connection, and does nothing until you use one of the supported commands.
+
+At this point, FizzTerm can save or load data to a SD card. For example, you might use it to save a BASIC program, and then load it later. Or you might even enter the program on your actual computer, and save it to the SD card so it can be loaded.
+
+## Hardware
+
+The full list of parts is:
+
+* An Arduino MEGA2560 or compatible
+* An Arduino-compatible SD-Card reader. (These are available on Amazon at 5 for $10)
+* Two Arduino-compatioble RS232-TTL adpators. (These are also available on Amazon for about $10 each)
+* An extra serial cable.
+
+Connect the SD-Card reader in the standard way (to CS, MSIO, MOSI, SCK, +3.3v and Gnd) and the two serial adpators to 16,17 and 18,19 (and 3.3v and Gnd).
+
+![](FizzTerm_bb.png)
+
+## Software
+
+The source code for FizzTerm should work as-in on any Arduino MEGA (the MEGA, or compatible, is required because of the improved Serial port support).
+The one caveat is when using the SD.H library. By defailt, the SD.H library will send the results of a directory list to Serial, and we would like it sent to Serial1.
+The simplest way around this is to copy the SD.H library, and then search and replace for Serial. in the source code. Or just don't just the *!LIST* command - other commands will work fine.
+
+
+## Operation
+
+Disconnect your serial connection between your retro-computer and your terminal. Connect the terminal to Serial port 1 of the FizzTerm, and the retro-computer to Serial Port 2.
+
+If you have connected all the RS232 cables correctly, and have all the RX/TX connections the right way around, when you power up the Arduino, you should see a welcome message. If not, confirm all the connections and check the baud rate (by default 9600).
+
+You are now free to use the retro-computer as you usually would. You should tot notice any difference (except that you can use backspace in the original Microsoft BASIC application - you're welcome), until you use a FizzTerm command, for example, !HELP.
+
+FizzTerm commands all start with !, and will send text only to the terminal (except !LOAD of course).
+
+For example, if you have a BASIC program you would like to save, you would perform the following steps.
+
+1. Make sure the BASIC program is ready on your retro-computer.
+2. Enter *!SAVE FILENAME.BAS*
+3. Type *LIST*
+4. Type *!STOP* or *CTRL-C*
+
+The program should be stored on the SD card. You can confirm by typing:
+
+*!LIST*
+
+and then
+
+*!DUMP FILENAME.BAS*
+
+To re-load the BASIC program:
+
+1. Type *NEW* on the retro-computer
+2. Type *!LOAD FILENAME.BAS*
+
+You should be now able to type *LIST* and see the listing as if you had typed it.
+
+
